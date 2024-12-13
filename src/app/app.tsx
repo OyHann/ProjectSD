d9aff1cac51ddbaa1d096e5cf09cf35ecc1c154c
Muhammad Fariska Raihan <88713553+OyHann@users.noreply.github.com>
2024-12-14 06:54:21 +0700

    menit 44

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,9 +2,9 @@
 import {store} from "@/redux/store";
 import React from 'react';
 import { Provider } from 'react-redux';
-const App = ({Children}:{Children: React.ReactNode} ) => {
+const App = ({children}:{children: React.ReactNode} ) => {
   return (
-    <Provider store={store}>{Children}</Provider>
+    <Provider store={store}>{children}</Provider>
   )
 }
 
